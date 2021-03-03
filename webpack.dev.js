af6a2506a76bfa8ae6224cadd402e80712a7696c
William Rojas <william.ur@live.com>
2021-03-02 21:35:52 -0700

    added style-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,6 +38,10 @@ module.exports = {
                     ]
              },
              {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+              },
+             {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                   {
